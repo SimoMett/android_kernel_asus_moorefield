1bd6bb33c801ca8cadc135b447cde43d1e4be64d
DonSimonetti <matt.simonetti@hotmail.com>
2023-02-01 10:47:27 +0100

    Extracting only jbd_state_bits instead

--- include/linux/jbd2.h
+++ include/linux/jbd2.h
@@ -309,20 +309,7 @@ typedef struct journal_superblock_s
 #include <linux/fs.h>
 #include <linux/sched.h>
 
-enum jbd_state_bits {
-	BH_JBD			/* Has an attached ext3 journal_head */
-	  = BH_PrivateStart,
-	BH_JWrite,		/* Being written to log (@@@ DEBUGGING) */
-	BH_Freed,		/* Has been freed (truncated) */
-	BH_Revoked,		/* Has been revoked from the log */
-	BH_RevokeValid,		/* Revoked flag is valid */
-	BH_JBDDirty,		/* Is dirty but journaled */
-	BH_State,		/* Pins most journal_head state */
-	BH_JournalHead,		/* Pins bh->b_private and jh->b_bh */
-	BH_Shadow,		/* IO on shadow buffer is running */
-	BH_Verified,		/* Metadata block has been verified ok */
-	BH_JBDPrivateStart,	/* First bit available for private use by FS */
-};
+#include <linux/jbd_common.h>
 
 BUFFER_FNS(JBD, jbd)
 BUFFER_FNS(JWrite, jwrite)
@@ -335,8 +322,6 @@ TAS_BUFFER_FNS(RevokeValid, revokevalid)
 BUFFER_FNS(Freed, freed)
 BUFFER_FNS(Shadow, shadow)
 BUFFER_FNS(Verified, verified)
-
-#include <linux/jbd_common.h>
 
 #define J_ASSERT(assert)	BUG_ON(!(assert))
 
--- include/linux/jbd_common.h
+++ include/linux/jbd_common.h
@@ -2,7 +2,21 @@
 #define _LINUX_JBD_STATE_H
 
 #include <linux/bit_spinlock.h>
-#include "jbd2.h"
+
+enum jbd_state_bits {
+    BH_JBD			/* Has an attached ext3 journal_head */
+    = BH_PrivateStart,
+    BH_JWrite,		/* Being written to log (@@@ DEBUGGING) */
+    BH_Freed,		/* Has been freed (truncated) */
+    BH_Revoked,		/* Has been revoked from the log */
+    BH_RevokeValid,		/* Revoked flag is valid */
+    BH_JBDDirty,		/* Is dirty but journaled */
+    BH_State,		/* Pins most journal_head state */
+    BH_JournalHead,		/* Pins bh->b_private and jh->b_bh */
+    BH_Shadow,		/* IO on shadow buffer is running */
+    BH_Verified,		/* Metadata block has been verified ok */
+    BH_JBDPrivateStart,	/* First bit available for private use by FS */
+};
 
 static inline struct buffer_head *jh2bh(struct journal_head *jh)
 {
